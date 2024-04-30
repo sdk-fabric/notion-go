9ebc1012d6b91cdbf06cb5a90b854a5c92e4e572
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-04-30 22:39:44 +0200

    initial commit

---
+++ client_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/sdk-fabric/notion-go/sdk"
+	"github.com/apioo/sdkgen-go"
+	"testing"
+)
+
+func TestClient(t *testing.T) {
+
+	client, err := sdk.Build("my_token")
+	if err != nil {
+		t.Error(err)
+	}
+
+}
